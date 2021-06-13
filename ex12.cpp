40ca64c66badc7dc2313d55d25a9f33153f88c10
Diba Mirza <dimirza@cs.ucsb.edu>
2021-06-12 20:20:47 -0700

    Create ex12.cpp

---
+++ ex12.cpp
@@ -0,0 +1,141 @@
+#include <iostream>
+#include <queue>
+#include <list>
+#include <stack>
+using namespace std;
+
+
+
+class smartQueue {
+    public:
+        void pop();
+        int front();
+        void push(int value);
+        bool empty();
+        int min();
+        int max();
+    private:
+        queue<int> q;
+        vector<int> v;
+};
+
+void smartQueue::push(int value) {
+    q.push(value);
+    v.push_back(value);
+}
+
+void smartQueue::pop() {
+    q.pop();
+    v.erase(v.begin());
+}
+
+int smartQueue::front() {
+    return q.front();
+}
+
+int smartQueue::min() {
+    int small{INT32_MAX};
+    for (int i = 1; i < v.size(); i++)
+        if (v[i] < small)
+            small = v[i];
+    return small;
+}
+
+int smartQueue::max() {
+    int large {-INT32_MAX};
+    for (int i = 1; i < v.size(); i++)
+        if (v[i] > large)
+            large = v[i];
+    return large;
+}
+
+bool smartQueue::empty() {
+    return q.empty();
+}
+
+int main() {
+    smartQueue sQ;
+    // push 3, 7, 2, 9, 8, 4
+    // pop() 
+    sQ.push(3);
+    cout<<"Push 3:"<<endl;
+    cout<<" Expected, Actual"<<endl;
+    cout<<" front: 3, "<<sQ.front()<<endl;
+    cout<<"   min: 3, "<<sQ.min()<<endl;
+    cout<<"   max: 3, "<<sQ.max()<<endl;
+   
+    sQ.push(7);
+    cout<<"Push 7"<<endl;
+     cout<<" Expected, Actual"<<endl;
+    cout<<" front: 3, "<<sQ.front()<<endl;
+    cout<<"   min: 3, "<<sQ.min()<<endl;
+    cout<<"   max: 7, "<<sQ.max()<<endl;
+    sQ.push(2);
+    cout<<"Push 2"<<endl;
+    cout<<" Expected, Actual"<<endl;
+    cout<<" front: 3, "<<sQ.front()<<endl;
+    cout<<"   min: 2, "<<sQ.min()<<endl;
+    cout<<"   max: 7, "<<sQ.max()<<endl;
+    sQ.push(9);
+    cout<<"Push 9"<<endl;
+    cout<<" Expected, Actual"<<endl;
+    cout<<"front: 3, "<<sQ.front()<<endl;;
+    cout<<"  min: 2, "<<sQ.min()<<endl;
+    cout<<"  max: 9, "<<sQ.max()<<endl;
+    sQ.push(8);
+    cout<<"Push 8"<<endl;
+    cout<<" Expected, Actual"<<endl;
+    cout<<" front: 3, "<<sQ.front()<<endl;
+    cout<<"   min: 2, "<<sQ.min()<<endl;
+    cout<<"   max: 9, "<<sQ.max()<<endl;
+    sQ.push(4);
+    cout<<"Push 4"<<endl;
+    cout<<" Expected, Actual"<<endl;
+    cout<<" front: 3, "<<sQ.front()<<endl;
+    cout<<"   min: 2, "<<sQ.min()<<endl;
+    cout<<"   max: 9, "<<sQ.max()<<endl;
+
+    cout<<endl<<"Start popping"<<endl<<endl;
+    // elements in smartQueue: 3, 7, 2, 9, 8, 4
+    sQ.pop();
+    cout<<"Popped 3"<<endl;
+    cout<<" Expected, Actual"<<endl;
+    cout<<" front: 7, "<<sQ.front()<<endl;
+    cout<<"   min: 2, "<<sQ.min()<<endl;
+    cout<<"   max: 9, "<<sQ.max()<<endl;
+    sQ.pop();
+    cout<<"Popped 7"<<endl;
+    cout<<" Expected, Actual"<<endl;
+    cout<<" front: 2, "<<sQ.front()<<endl;
+    cout<<"   min: 2, "<<sQ.min()<<endl;
+    cout<<"   max: 9, "<<sQ.max()<<endl;
+    sQ.pop();
+    cout<<"Popped 2"<<endl;
+    cout<<" Expected, Actual"<<endl;
+    cout<<" front: 9, "<<sQ.front()<<endl;
+    cout<<"   min: 4, "<<sQ.min()<<endl;
+    cout<<"   max: 9, "<<sQ.max()<<endl;
+    sQ.pop();
+    cout<<"Popped 9"<<endl;
+    cout<<" Expected, Actual"<<endl;
+    cout<<" front: 8, "<<sQ.front()<<endl;
+    cout<<"   min: 4, "<<sQ.min()<<endl;
+    cout<<"   max: 8, "<<sQ.max()<<endl;
+    cout<<"Print rest of the queue: ";
+    sQ.push(3);
+    cout<<"Pushed 3"<<endl;
+    // queue has 8, 4, 3
+    cout<<" Expected, Actual"<<endl;
+    cout<<" front: 8, "<<sQ.front()<<endl;
+    cout<<"   min: 3, "<<sQ.min()<<endl;
+    cout<<"   max: 8, "<<sQ.max()<<endl;
+    sQ.pop();
+    cout<<"Popped 8"<<endl;
+    cout<<" Expected, Actual"<<endl;
+    cout<<" front: 4, "<<sQ.front()<<endl;
+    cout<<"   min: 3, "<<sQ.min()<<endl;
+    cout<<"   max: 4, "<<sQ.max()<<endl;
+    cout<<"Print rest of the queue: ";
+   // print_queue(sQ);
+    return 0;
+}
